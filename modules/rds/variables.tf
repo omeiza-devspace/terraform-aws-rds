e6b0fed3f5fa455067fa5bdd73fd980e486aca74
Omeiza Okuta <okuta.omeiza@gmail.com>
2024-01-11 19:50:20 +0000

    improved code security

--- modules/rds/variables.tf
+++ modules/rds/variables.tf
@@ -23,19 +23,16 @@ variable "aws_vpc_id" {
 variable "db_engine" {
   type        = string
   description = "database engine type"
-  default     = "mysql"
 }
 
 variable "db_engine_version" {
   type        = string
   description = "database engine version"
-  default     = "5.7"
 }
 
 variable "db_engine_family" {
   type        = string
   description = "database engine family"
-  default     = "5.7"
 }
 
 ## RDS variables
@@ -53,25 +50,23 @@ variable "db_subnet_group_ids" {
 variable "db_name" {
   description = "Specifies the RDS name of the db"
   type        = string
-  default     = "demo-db"
 }
 
 variable "db_username" {
   description = "Specifies if the RDS instance is multi-AZ"
   type        = string
-  default     = "dbAdmin"
+  sensitive   = true
 }
 
 variable "db_password" {
   description = "Specifies if the RDS instance is multi-AZ"
   type        = string
-  default     = "password234-1"
+  sensitive   = true
 }
 
 variable "db_port" {
   description = "Specifies if the RDS access port"
   type        = string
-  default     = "3306"
 }
 
 variable "db_maintenance_window" {
@@ -82,7 +77,6 @@ variable "db_maintenance_window" {
 variable "email_endpoint" {
   description = "Specifies SNS email address"
   type        = string
-  default     = "admin@localhost.com"
 }
 
 
--- variables.tf
+++ variables.tf
@@ -1,24 +1,94 @@
 
-## Project Variables
-variable "region" {}
-variable "project" {}
-variable "env" {}
-variable "profile" {}
+###########################
+######### PROJECT #########
+###########################
+variable "region" {
+    description = "AWS service region"
+    type = "string"
+}
+
+variable "project" {
+    description = "Project name"
+    type = "string"
+}
+variable "env" {
+    description = "Deployment environment"
+    type = "string"
+}
+variable "profile" {
+    description = "AWS service profile"
+    type = "string"
+}
 
 
-## VPC variables
-variable "vpc_cidr_block" {}
-variable "public_subnets_cidr_block" {}
-variable "private_subnets_cidr_block" {}
-variable "public_subnet_az" {}
-variable "private_subnet_az" {}
+###########################
+########### VPC ###########
+###########################
+variable "vpc_cidr_block" {
+    description = "AWS VPC CIDR block"
+    type = "string"
+}
 
-## RDS Variables
-variable "rds_username" {}
-variable "rds_password" {}
-variable "rds_port" {}
-variable "rds_dbname" {}
-variable "rds_db_engine" {}
-variable "rds_db_family" {}
-variable "rds_db_version" {}
-variable "rds_maintenance_window" {}
+variable "public_subnets_cidr_block" {
+    description = "AWS VPC public subnet CIDR"
+    type = "string"
+}
+
+variable "private_subnets_cidr_block" {
+    description = "AWS VPC private subnet CIDR"
+    type = "string"
+}
+
+variable "public_subnet_az" {
+    description = "AWS VPC Private Subnet Acailability Zone"
+    type = "string"
+}
+
+variable "private_subnet_az" {
+    description = "AWS VPC Public Subnet Acailability Zone"
+    type = "string"
+}
+
+###########################
+########### RDS ###########
+###########################
+variable "rds_username" {
+    description = "RDS Master username "
+    type = "string"
+    sensitive = true
+}
+
+variable "rds_password" {
+    description = "RDS Master password "
+    type = "string"
+    sensitive = true
+}
+
+variable "rds_port" {
+    description = "RDS port"
+    type = "string"
+}
+
+variable "rds_dbname" {
+    description = "RDS database name"
+    type = "string"
+}
+
+variable "rds_db_engine" {
+    description = "RDS database engine name"
+    type = "string"
+}
+variable "rds_db_family" {
+    description = "RDS database family"
+    type = "string"
+}
+
+variable "rds_db_version" {
+   description = "RDS database version"
+   type = "string"
+}
+
+variable "rds_maintenance_window" {
+    description = "RDS database maintenance period"
+    type = "string"
+}
